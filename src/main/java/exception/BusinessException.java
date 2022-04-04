9e08a5e835c41d4734bdc4fb296c92688e1e888e
anisha12 <banisha16@gmail.com>
2022-04-04 20:27:10 +0530

    custom exception added

---
+++ src/main/java/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String msg){
+        super(msg);
+    }
+    public BusinessException(String msg,Throwable cause){
+        super(msg,cause);
+    }
+}
